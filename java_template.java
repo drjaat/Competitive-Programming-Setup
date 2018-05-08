8f4bd97be00e850117e21a1e06f3cbdb00a8ff33
Dhruv Rana <22201670+drjaat@users.noreply.github.com>
2018-05-08 22:41:33 +0530

    Update java_template.java

--- java_template.java
+++ java_template.java
@@ -1,50 +1,81 @@
-import java.io.*;
+/*
+    ######  ######    #####  ######  ######  #####
+    #    #  #    #      #    #    #  #    #    #
+    #    #  # #  #      #    # ## #  # ## #    #
+    #    #  #  #    #   #    #    #  #    #    #
+    ######  #   #   # # #    #    #  #    #    #
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 import java.math.*;
- 
-public class Main {
-    public static void main(String[] args) throws Exception  {
-        InputStream inputStream = System.in;
-        OutputStream outputStream = System.out;
-        InputReader in = new InputReader(inputStream);
-        PrintWriter out = new PrintWriter(outputStream);
-        Task solver = new Task();
-        int tesCases = in.nextInt();
-        for(int test = 1; test <= tesCases; test += 1) {
-            solver.solve(test, in, out);
-        }
-        out.close();
-    }
+import java.util.StringTokenizer;
+class Clone {
+    static PrintStream p = System.out;
     
-    static class Task {
-        static final int MOD = (int) (1e9 + 7);
+    public static void main(String args[] ) throws Exception {
+        int n = ni(); 
+        int arr[] =  inta(n);
+        pia(arr);
 
-        public void solve(int testNumber, InputReader in, PrintWriter out) {
-            //Your code goes below
-
-        }
     }
 
-    static class InputReader {
-        public BufferedReader take;
-        public StringTokenizer split;
-        public InputReader(InputStream stream) {
-            take = new BufferedReader(new InputStreamReader(stream), 10000000);
-            split = null;
-        }
-        public String next() {
-            while (split == null || !split.hasMoreTokens()) {
-                try {
-                    split = new StringTokenizer(take.readLine());
-                } 
-                catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+
+
+//-------------------------------------------------fast Method---------------------------------------------------------------\\
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+    static StringTokenizer st;
+    private static int[] inta(int n){
+        int [] a = new int[n];
+        for(int i = 0;i < n;i++)a[i] = ni();
+            return a;
+    }
+    private static long[] longa(long n){
+    long[] a = new long[(int)n];
+        for(int i = 0;i < n;i++)a[i] = nl();
+            return a;
+    }
+    private static void pla(long[] a){
+        for(int i = 0;i <a.length;i++)
+            p.print(a[i]+" ");
+    }
+    private static void pia(int[] a){
+        for(int i = 0;i <a.length;i++)
+            p.print(a[i]+" ");
+    }
+    private static String n(){
+        while (st == null || !st.hasMoreElements()){
+            try{
+                st = new StringTokenizer(br.readLine());
             }
-            return split.nextToken();
+            catch (IOException  e){
+                e.printStackTrace();
+            }
         }
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
+        return st.nextToken();
     }
-}
+    private static int ni(){
+        return Integer.parseInt(n());
+    }
+
+    private static long nl(){
+        return Long.parseLong(n());
+    }
+
+    private static double nd(){
+        return Double.parseDouble(n());
+    }
+    private static String nli(){
+        String str = "";
+        try{
+            str = br.readLine();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return str;
+    }
+//-------------------------------------------------fast Method---------------------------------------------------------------\\
+} 
